f3dd3d262b8d2dba9c26f4f10ada865c9182ab79
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-02-01 18:34:26 +0100

    add add-employee feature

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -29,5 +32,21 @@ public class EmployeeController {
         model.addAttribute("employees", employees);
 
         return "list-employees";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model) {
+
+        model.addAttribute("employee", new Employee());
+
+        return "save-employee";
+    }
+
+    @PostMapping("/")
+    public String addEmployee(@ModelAttribute Employee employee, Model model) {
+
+        employeeService.save(employee);
+
+        return listEmployees(model);
     }
 }
